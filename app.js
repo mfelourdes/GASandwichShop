1b08a62bcc58c833863002fccf41e53ebee6b68a
mfelourdes <91541165+mfelourdes@users.noreply.github.com>
2022-01-06 12:49:47 +0800

    GASandwich

--- app.js
+++ app.js
@@ -1,301 +1,323 @@
-console.log('hi')
-
-
-const $div = $('<div>').attr('id', 'HamNCheese').addClass("HamNCheese")
-
-const $div1 = () => {
-    for (i=0; i<ingredients.HamNCheese.length; i++) {
-        div2 = $('<div>').append(i).text(ingredients.HamNCheese[i]).addClass('HM').addClass(ingredients.HamNCheese[i])
-        $div.append(div2)
-    }
-}
-
-const $div3 = $('<div>').attr('id', 'pbj').addClass('pbj')
-
-const $div4 = () => {
-    for (i=0; i<ingredients.PBJ.length; i++) {
-        div4= $('<div>').append(i).text(ingredients.PBJ[i]).addClass('PBJ').addClass(ingredients.PBJ[i])
-        $div3.append(div4)
-    }
-}
-
-const $div2 = $('<div>').attr('id', 'prepBoard').addClass('Board').text('Board')
-
-const $div6 = $('<div>').attr('id', 'prepDone').addClass('prepDone')
-
-const $div5 = $('<div>').attr('id', 'OrderTicket').addClass('OrderTicket')
-
-const $div7 = $('<div>').attr('id', 'Bill').addClass('Bill').text('$').hide()
-
-const $div8 = $('<div>').attr('id', 'Cash').addClass('Cash')
-
-const $div9 = $('<div>').attr('id', 'OT').addClass('OT')
-
-const $div10 = $('<div>').attr('id', 'HMSandwich').addClass('HMSandwich').hide()
-
-const $div11 = $('<div>').attr('id', 'PBJSandwich').addClass('PBJSandwich').hide()
-
-const $div12 = $('<div>').attr('id','gameTime').addClass('gameTime')
-
-const $div13 = $('<div>').attr('id', 'shopClosed').addClass('shopClosed').text('Shop Closed').hide()
- 
-const $div14 = $('<div>').attr('id', 'HamIMG').addClass('HamIMG').text('HM').hide()
-
-const $div15 = $('<div>').attr('id', 'pbjIMG').addClass('pbjIMG').text('PBJ').hide()
-
+console.log("hi");
 
 const computer = {
-    order: ''
-}
+  order: "",
+};
 
 const player = {
-    prepare: null
-}
+  drinkorder: "",
+};
 
-const menu = ["HamNCheese", "PBJ"]
+const menu = ["HamNCheese", "PBJ", "Baguette"];
 
 const ingredients = {
-    HamNCheese: ['brioche', 'ham', 'gouda'],
-    PBJ: ["whole wheat", "peanut butter", "blueberry jam"]
+  HamNCheese: ["brioche", "ham", "gouda"],
+  PBJ: ["sourdough", "peanut butter", "blueberry jam"],
+  Baguette: ["baguette", "butter", "sea salt"],
+};
+
+const drinks = ["cappucino", "matchaLatte"];
+
+const drinkRecipe = {
+    cappucino: ["espresso", "milk", "foam"],
+    matchaLatte: ["matcha", "soy milk"]
+
 }
 
 const price = {
-    HamNCheese: 6,
-    PBJ: 5
-}
+  HamNCheese: 6,
+  PBJ: 5,
+  Baguette: 4,
+  cappucino: 4,
+  matchaLatte: 3,
+};
 
 const computerChoice = () => {
-    const randomChoice = Math.floor(Math.random() * menu.length)
-    computer.order = menu[randomChoice]
-    //alert(computer.order)
-    $('.OT').text(computer.order)
-}
+  const randomChoice = Math.floor(Math.random() * menu.length);
+  computer.order = menu[randomChoice];
+  $(".OT").text(computer.order);
+};
 
-let list = []
-const makeSandwich = () => {
-    computerChoice()
-    
-    if(computer.order === menu[0]) {
-            $('.HM').on('click', (event)=>{  
-                const $target = $(event.target) 
-                list.push($target.text())
-                 console.log(list)
-                 if (list.length === 3 && list[0] === a[0] && list[1] === a[1] && list[2] === a[2]) {
-                     $('.Board').on('click', ()=> {
-                        $('.HamIMG').show()
-            
-                     }) 
-                     } 
-
-            })
-         
- 
-    }
-
-   
-    if (computer.order === menu[1]) {
-        $('.PBJ').on('click', (event)=>{
-            const $target1 = $(event.target)
-            list.push($target1.text())
-                 console.log(list)
-                 if (list.length === 3 &&  list[0] === b[0] && list[1] === b[1] && list[2]===b[2] ) {
-                     $('.Board').on('click', ()=> {
-                        $('.pbjIMG').show()
-                     }) 
-                     } 
-        
-        })
-   
-    }
-    
-    
-    
-}
+const playerChoice = () => {
+  const randomChoice1 = Math.floor(Math.random() * drinks.length);
+  player.drinkorder = drinks[randomChoice1];
+  $(".DO").text(player.drinkorder);
+};
 
 
 
-const packSandwich = () => {
-}
-
-
-// let result =
-//   (ingredients.HamNCheese).every(function (element) {
-//     return list.includes(element);
-//   });
-
-
-const a = ingredients.HamNCheese
-const b = ingredients.PBJ
-// let result =
-//   (a).every(function (i) {
-//     return list.includes(i);
-//     });
-        
-//  const compareH = () => {
-//     list[0] === a[0] && list[1] === a[1] && list[2] === a[2]  
-//  }
-
-
-//  const compareP = () => {
-//     list[0] === b[0] && list[1] === b[1] && list[2]===b[2]
-//  }
-
-
- const emptylist = () => {
-    list = []
-    console.log(list)
-
-}
+let list = [];
+const makeHMC = (event) => {
+    const $target = $(event.target);
+    list.push($target.text());
+    $(".prepBoard").text(list);
+    console.log(list);
   
-const giveOrder = () => {
-        //$('.prepDone').empty() 
-        emptylist()
-        $('.HamIMG').hide()
-        $('.pbjIMG').hide()
+    if (
+      list.length === 3 &&
+      list[0] === a[0] &&
+      list[1] === a[1] &&
+      list[2] === a[2]
+    ) {
+      $(".prepDone").text("order done");
     }
-    
+  };
+  
+  const makePBJ = (event) => {
+    const $target = $(event.target);
+    list.push($target.text());
+    $(".prepBoard").text(list);
+    console.log(list);
+    if (
+      list.length === 3 &&
+      list[0] === b[0] &&
+      list[1] === b[1] &&
+      list[2] === b[2]
+    ) {
+      $(".prepDone").text("order done");
+    }
+  };
+  
+  const makeBaguette = (event) => {
+      const $target = $(event.target);
+    list.push($target.text());
+    $(".prepBoard").text(list);
+    console.log(list);
+    if (
+      list.length === 3 &&
+      list[0] === c[0] &&
+      list[1] === c[1] &&
+      list[2] === c[2]
+    ) {
+      $(".prepDone").text("order done");
+    }
+  
+  }
+
+
+  const makeCap = (event) => {
+    const $target = $(event.target);
+    list.push($target.text());
+    $(".prepBoard").text(list);
+    console.log(list);
+    if (
+      list.length === 3 &&
+      list[0] === d[0] &&
+      list[1] === d[1] &&
+      list[2] === d[2]
+    ) {
+      $(".prepDone").text("order done");
+    }
+
+}
+
+
+const makeMatcha = (event) => {
+    const $target = $(event.target);
+    list.push($target.text());
+    $(".prepBoard").text(list);
+    console.log(list);
+    if (
+      list.length === 2 &&
+      list[0] === d[0] &&
+      list[1] === d[1] 
+    ) {
+      $(".prepDone").text("order done");
+    }
+
+}
+
+
+
+const a = ingredients.HamNCheese;
+const b = ingredients.PBJ;
+const c = ingredients.Baguette;
+const d = drinkRecipe.cappucino
+const e = drinkRecipe.matchaLatte
+
+
 
 
 const completeOrder = () => {
-   
-        sandwichOrder()
-        $('.Bill').show()
-    
-        
-    
-}
+  $(".collectCash").show();
+  $(".prepDone").empty();
+  
+};
 
 const collectMoney = () => {
-        earnCash()
-        $('.Bill').hide()
-        $('.HMSandwich').hide()
-        $('.PBJSandwich').hide()
-        
-}
+  earnCash();
+  $(".collectCash").hide();
+  $(".HMC").hide();
+  $(".PBJS").hide();
+  $(".BAGS").hide();
+  $(".COFFEE").hide();
+  $(".TEA").hide();
+};
 
 const sandwichOrder = () => {
-
-        if(computer.order === menu[0])  {
-            $('.HMSandwich').show()
+        if (computer.order === menu[0]) {
+            $(".HMC").show();
+          }
+        
+          if (computer.order === menu[1]) {
+            $(".PBJS").show();
+          }
+        
+          if (computer.order === menu[2]) {
+            $(".BAGS").show();
     }
-    
-    
+  
 
-    if(computer.order === menu[1]) {
-        $('.PBJSandwich').show()
-    }
+  
 
+};
+
+const drinkPrep = () => {
+    if (player.drinkorder === drinks[0]) {
+        $(".COFFEE").show();
+      }
+
+      if (player.drinkorder === drinks[1]) {
+        $(".TEA").show();
+      }
 }
 
+const sandOrder = () => {
+    sandwichOrder()
+    $(".prepBoard").empty();
+     list = [];
+    
+}
 
+const drinkOrder = () => {
+    drinkPrep()
+    $(".prepBoard").empty();
+    list = [];
+}
 
-let cash = 0
+let cash = 0;
 const earnCash = () => {
-    if(computer.order === menu[0]) {
-        const cashIncrement = price.HamNCheese
-        cash = cash + cashIncrement
-    }
+  if (computer.order === menu[0]) {
+    const cashIncrement = price.HamNCheese;
+    cash = cash + cashIncrement;
+  } else if (computer.order === menu[1]) {
+    const cashIncrement = price.PBJ;
+    cash = cash + cashIncrement;
+  } else if (computer.order === menu[2]) {
+    const cashIncrement = price.Baguette;
+    cash = cash + cashIncrement;
+  }
+  if (player.drinkorder === drinks[0]){
+    const cashIncrement = price.cappucino;
+    cash = cash + cashIncrement;
 
-   else if(computer.order === menu[1]) {
-        const cashIncrement = price.PBJ
-        cash = cash + cashIncrement
-    }
-    $div8.text(cash)
-    
-    makeSandwich()
-   
-    
-   
-}
+  }
+  if (player.drinkorder === drinks[1]){
+    const cashIncrement = price.matchaLatte;
+    cash = cash + cashIncrement;
 
+  }
+
+
+  $(".Earnings").text(` $${cash}`);
+
+  computerChoice();
+  playerChoice();
+};
 
 //const myInterval = setInterval(earnCash, 10000)
 
 // const myStop = () => {
 //     clearInterval(myInterval)
 // }
-const counter = 0
-let time = 60
+const counter = 0;
+let time = 60;
 
 const gameTime = () => {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
 
-    const minutes = Math.floor(time/60);
-    let seconds = time % 60;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  $(".gameTime").text(`${minutes}:${seconds}`);
 
-    seconds = seconds < 10 ? '0' + seconds: seconds
-    $('.gameTime').text(`${minutes}:${seconds}`)
-    
-    time --
+  time--;
 
-    if (time===0)  {
-        clearInterval(start)
-        $('.shopClosed').show()
-    }
-}
-
-
-
-
- 
-
-
-
+  if (time === 0) {
+    clearInterval(start);
+    $(".shopClosed").show();
+  }
+};
 
 // const endGame = () => {
 //     if (time <= 0) {
 //         clearInterval(startGame)
 //     }
-    
+
 // }
 
-const main = () => {
-
-$('body').append($div5)
-$('body').append($div7)
-$('body').append($div12)
-$('body').append($div)
-$('body').append($div3)
-$('body').append($div2)
-$('body').append($div8)
-$('body').append($div13)
-
-$div2.append($div6)
-
-$div5.append($div9)
-$div5.append($div10)
-$div5.append($div11)
-$div2.append($div14)
-$div2.append($div15)
 
 
 
 
-$div1()
-$div4()
 
 
-makeSandwich()
-
-$('.OT').on('click',giveOrder)
-emptylist()
-$('.OrderTicket').on('click', completeOrder)
-$('.Bill').on('click',collectMoney)
-
+const $button = $("<button>").addClass("HMC").hide().text("HMC");
+const $button1 = $("<button>").addClass("PBJS").hide().text("PBJS");
+const $button2 = $("<button>").addClass("BAGS").hide().text("BAGS");
+const $button3 = $("<button>").addClass("COFFEE").hide().text("CAPUCCINO");
+const $button4 = $("<button>").addClass("TEA").hide().text("TEA");
 
 
-//startGame()
-
-//start = setInterval(gameTime, 1000)
-
-
-//console.log(console.log(list) === console.log(ingredients.HamNCheese))
-
-
-
-//console.log(result); // true
-
+const unclick = (event) => {
+    $target = $(event.target)
 
 }
-$(main)
+
+const bin = (event) => {
+    $target = $(event.target)
+    list =[]
+    $('.prepBoard').empty()
+
+}
 
 
+
+const main = () => {
+  $("#orderComplete").append($button);
+  $("#orderComplete").append($button1);
+  $("#orderComplete").append($button2);
+  $("#orderComplete").append($button3);
+  $("#orderComplete").append($button4);
+
+  computerChoice();
+  playerChoice();
+ 
+
+  $(".OC").on("click", completeOrder);
+  $(".collectCash").on("click", collectMoney);
+  $(".HM").on("click", makeHMC);
+  $(".PBJ").on("click", makePBJ);
+  $(".BAG").on("click", makeBaguette)
+
+  $(".coffee").on("click", makeCap)
+  $(".tea").on("click", makeMatcha)
+
+
+  $(".drinkOC").on("click", drinkOrder)
+  $(".sandOC").on("click",sandOrder)
+
+  $('.btn').on("click", unclick)
+  $('.bin').on("click", bin)
+
+
+
+
+  //startGame()
+
+  //start = setInterval(gameTime, 1000)
+
+  //console.log(console.log(list) === console.log(ingredients.HamNCheese))
+
+  //console.log(result); // true
+};
+$(main);
--- app1.js
+++ app1.js
@@ -1,1 +1,49 @@
 console.log('hello')
+
+// const dishes = {
+//     hamAndCheese: {
+//       ingredients: ["brioche", "ham", "gouda"],
+//       price: 2,
+//     },
+//     pbj: {
+//       ingredients: ["whole wheat", "peanut butter", "blueberry jam"],
+//       price: 3,
+//     },
+//   };
+  
+//   let prepboard = ["whole wheat", "peanut butter"];
+  
+//   console.log();
+  
+//   const checkOrder = (list, a) => {
+//     console.log(list.join());
+//     console.log(a.join());
+//     if (board.join() === dish.join()) {
+//       return console.log("order complete!");
+//     }
+//     return console.log("wrong items!");
+//   };
+  
+
+
+//   checkOrder(prepboard, dishes.pbj.ingredients);
+
+// let result =
+//   (a).every(function (i) {
+//     return list.includes(i);
+//     });
+        
+//  const compareH = () => {
+//     list[0] === a[0] && list[1] === a[1] && list[2] === a[2]  
+//  }
+
+
+//  const compareP = () => {
+//     list[0] === b[0] && list[1] === b[1] && list[2]===b[2]
+//  }
+
+
+// let result =
+//   (ingredients.HamNCheese).every(function (element) {
+//     return list.includes(element);
+//   });
